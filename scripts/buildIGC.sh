06f0510545930990f55e59b34b36ae7d287ec4ad
Michał Krause <michalkr53@gmail.com>
2025-10-24 13:56:58 +0200

    Update scripts, test

--- scripts/buildIGC.sh
+++ scripts/buildIGC.sh
@@ -2,32 +2,32 @@
 
 #=========================== begin_copyright_notice ============================
 #
-# Copyright (C) 2022 Intel Corporation
+# Copyright (C) 2022-2025 Intel Corporation
 #
 # SPDX-License-Identifier: MIT
 #
 #============================ end_copyright_notice =============================
 
 set -e
-# UBUNTU_VERSION   supported value [ 20, 22 ]                                                default 20
-# LLVM_VERSION     supported value [ 10, 11, 12, 13, 14, 15]                                 default 11
+# UBUNTU_VERSION   supported value [ 22.04, 24.04 ]                                          default 24.04
+# LLVM_VERSION     supported value [ 14, 15, 16 ]                                            default 16
 # COMPILER         supported value [ gcc, clang ]                                            default gcc
 # OWN_CMAKE_FLAGS  not suported but can be use as WA (each flag should be with -D prefix)    default empty
-# example run:     UBUNTU_VERSION=ubuntu2004 LLVM_VERSION=11 COMPILER=gcc sh /home/buildIGC.sh
+# example run:     UBUNTU_VERSION=24.04 LLVM_VERSION=16 COMPILER=gcc sh /home/buildIGC.sh
 
 echo "====================BUILD IGC========================="
 echo "[Build Status] build script started"
 if [ -z ${UBUNTU_VERSION+x} ]; then
-    echo "[Build Status] UBUNTU_VERSION is unset, use default 20";
+    echo "[Build Status] UBUNTU_VERSION is unset, use default 24.04";
     UBUNTU_VERSION="24.04"
 else
     echo "[Build Status] UBUNTU_VERSION = ${UBUNTU_VERSION}"
 fi
 if [ -z ${LLVM_VERSION+x} ]; then
-    echo "[Build Status] LLVM_VERSION is unset, use default 14";
+    echo "[Build Status] LLVM_VERSION is unset, use default 16";
     LLVM_VERSION="16"
 else
-    echo "[Build Status] LLVM_VERSION = ${LLVM_VERSION}" 
+    echo "[Build Status] LLVM_VERSION = ${LLVM_VERSION}"
 fi
 if [ -z ${COMPILER+x} ]; then
     echo "[Build Status] COMPILER is unset, use default gcc";
@@ -43,7 +43,7 @@ else
 fi
 if [ -z ${IGC_SHA+x} ]; then
     echo "[Build Status] IGC_SHA is unset, use default master";
-    OWN_CMAKE_FLAGS="master"
+    IGC_SHA="master"
 else
     echo "[Build Status] IGC_SHA = ${IGC_SHA}"
 fi
@@ -52,31 +52,18 @@ apt-get update
 apt-get install -y flex bison libz-dev cmake curl wget build-essential git software-properties-common unzip file lsb-release python3-mako libc6 libstdc++6 libzstd-dev
 echo "[Build Status] flex bison libz-dev cmake curl wget build-essential git software-properties-common unzip file INSTALLED"
 
-if [ "$UBUNTU_VERSION" = "20.04" ]; then
-    echo "[Build Status] Download new cmake version for Ubuntu 20.04";
-    apt-get purge -y --auto-remove cmake
-    wget -O - https://apt.kitware.com/keys/kitware-archive-latest.asc 2>/dev/null | gpg --dearmor - | tee /etc/apt/trusted.gpg.d/kitware.gpg >/dev/null
-    apt-add-repository "deb https://apt.kitware.com/ubuntu/ $(lsb_release -cs) main"
-    apt-get update
-    apt-get install -y cmake
-fi
-
-if ([ "$UBUNTU_VERSION" = "20.04" ] && [ "$LLVM_VERSION" -ge 14 ]) || ([ "$UBUNTU_VERSION" = "22.04" ] && [ "$LLVM_VERSION" -ge 15 ])
+if ([ "$UBUNTU_VERSION" = "22.04" ] && [ "$LLVM_VERSION" -ge 16 ])
 then
     echo "[Build Status] Retrieve the LLVM archive signature for LLVM $LLVM_VERSION on Ubuntu $UBUNTU_VERSION";
     wget -q https://apt.llvm.org/llvm-snapshot.gpg.key
     apt-key add llvm-snapshot.gpg.key
-    case "$UBUNTU_VERSION" in
-        20.04) OS_HANDLE=focal;;
-        22.04) OS_HANDLE=jammy;;
-    esac
-    add-apt-repository "deb http://apt.llvm.org/$OS_HANDLE/ llvm-toolchain-$OS_HANDLE-$LLVM_VERSION main"
+    add-apt-repository "deb http://apt.llvm.org/jammy/ llvm-toolchain-jammy-$LLVM_VERSION main"
 fi
 
 apt-get install -y llvm-"$LLVM_VERSION" llvm-"$LLVM_VERSION"-dev clang-"$LLVM_VERSION" liblld-"$LLVM_VERSION" liblld-"$LLVM_VERSION"-dev
 echo "[Build Status] LLVM INSTALLED"
 
-LLVM_VERSION_PREFERRED="$LLVM_VERSION".0.6
+LLVM_VERSION_PREFERRED="$LLVM_VERSION".0.0
 echo "[Build Status] LLVM_VERSION_PREFERRED = $LLVM_VERSION_PREFERRED"
 
 echo "[Build Status] Prepare install OpenCL Clang"
@@ -113,7 +100,7 @@ mkdir build && cd build
 cmake ../ $CONFIG_VARS
 echo "[Build Status] Cmake created"
 
-make VERBOSE=1 -j`nproc`
+make -j`nproc`
 echo "[Build Status] make DONE"
 cpack
 mkdir DEB-FILES && mv ./*.deb ./DEB-FILES
